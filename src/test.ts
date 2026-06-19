249f7555eba95b4ed0f0618f55d034631e8ddb28
Shivnu27 <manirathi2710.com>
2026-06-19 16:11:04 +0530

    Update

---
+++ src/test.ts
@@ -0,0 +1,7 @@
+let name: string = "mani";
+let exp: number = 5;
+const Company: string  = "Google";
+let isAutomationEngineer: boolean = true;
+let data: any = "Anything"
+name = "Shivnu";
+console.log(name, exp, Company, isAutomationEngineer, data);mkdir my-ts-project 
