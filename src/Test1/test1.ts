3a19ee9d60e2756825647589a8d074a1f9f541b1
Shivnu27 <manirathi2710.com>
2026-06-29 14:23:27 +0530

    TypeScript Debugging Assessment

--- src/Test1/test1.ts
+++ src/Test1/test1.ts
@@ -1,32 +1,33 @@
-//
-let employeeName: string = "Name: Mani";
-let employId: any = "ID: 1234";
-let department: string = "Department: QA Team";
-let salary: number = 44000;
-console.log(employeeName);
-console.log(employId);
-console.log(department);
-console.log(salary);
+// //
+// let employeeName: string = "Name: Mani";
+// let employId: any = "ID: 1234";
+// let department: string = "Department: QA Team";
+// let salary: number = 44000;
+// console.log(employeeName);
+// console.log(employId);
+// console.log(department);
+// console.log(salary);
 
-let bonus: number = 55000; 
-if (bonus >= 40000) { 
-console.log("Employee is eligible for bonus"); 
-} else { 
-console.log("Not eligible"); 
-}
+// let bonus: number = 55000; 
+// if (bonus >= 40000) { 
+// console.log("Employee is eligible for bonus"); 
+// } else { 
+// console.log("Not eligible"); 
+// }
 
-let experiance: number = 12; 
-if (experiance >= 5) { 
-console.log("Experiance is met for bonus"); 
-} else { 
-console.log("Junior"); 
-}
+// let experiance: number = 12; 
+// if (experiance >= 5) { 
+// console.log("Experiance is met for bonus"); 
+// } else { 
+// console.log("Junior"); 
+// }
 
-let location: string = "chennai"; 
-if (location == "chennai") {
-console.log("Employee is located in Chennai"); 
-} else { 
-console.log("Employe is not located in Chennai"); 
-}
+// let location: string = "chennai"; 
+// if (location == "chennai") {
+// console.log("Employee is located in Chennai"); 
+// } else { 
+// console.log("Employe is not located in Chennai"); 
+// }
 
-console.log(salary += bonus);
+// console.log(salary += bonus);
+
